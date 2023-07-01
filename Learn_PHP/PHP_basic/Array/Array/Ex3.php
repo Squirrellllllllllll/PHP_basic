d25d9932b1a301a05170e72553529dda896ae5c7
Squirrellllllllllll <90439584+Squirrellllllllllll@users.noreply.github.com>
2023-07-01 20:32:10 +0700

    Add files via upload

---
+++ Learn_PHP/PHP_basic/Array/Array/Ex3.php
@@ -0,0 +1,15 @@
+<?php
+
+// Hãy viết một trang web đơn giản để đăng ký người dùng. Trang web có một biểu mẫu đăng ký gồm các trường sau:
+
+// Tên đăng nhập (username): Một chuỗi có độ dài từ 5 đến 20 ký tự.
+// Mật khẩu (password): Một chuỗi có độ dài tối thiểu 8 ký tự và bao gồm ít nhất một chữ cái viết hoa, một chữ cái viết thường và một số.
+// Xác nhận mật khẩu (confirm password): Phải khớp với mật khẩu đã nhập.
+// Email: Phải là một địa chỉ email hợp lệ.
+// Yêu cầu:
+
+// Xử lý biểu mẫu và hiển thị thông báo lỗi nếu có.
+// Nếu không có lỗi, hiển thị thông báo đăng ký thành công và các thông tin đã được gửi đi.
+// Gợi ý:
+
+// Sử dụng các điều kiện (if-else, switch) để xử lý logic kiểm tra và hiển thị thông báo lỗi.
